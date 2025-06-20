6a01d0836b15aa68c97d37c1598f2da7682e0497
Sumit Kumar Yadav <profsumit2023@gmail.com>
2025-06-21 02:55:02 +0545

    feature to join as doctor or patient added

---
+++ app/(main)/doctors/[speciality]/page.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SpecialityPage = ({ params }) => {
+  const { speciality } = params;
+  return <div>specialtyPage : {speciality}</div>;
+};
+
+export default SpecialityPage;

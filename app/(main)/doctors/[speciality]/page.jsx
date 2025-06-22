97f5bb2ef034289118c182c4f07b193d417f53b2
Sumit Kumar Yadav <profsumit2023@gmail.com>
2025-06-22 21:24:09 +0545

    Doctors listing page added

--- app/(main)/doctors/[speciality]/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const SpecialityPage = ({ params }) => {
-  const { speciality } = params;
-  return <div>specialtyPage : {speciality}</div>;
-};
-
-export default SpecialityPage;
